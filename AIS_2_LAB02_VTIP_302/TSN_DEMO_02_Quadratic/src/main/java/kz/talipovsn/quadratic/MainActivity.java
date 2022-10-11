6d3b289d8a1cd9332d3c5b9eadfe933fe0a8a643
abeldinov160599 <71010650+abeldinov160599@users.noreply.github.com>
2022-10-11 10:30:29 +0600

    Add files via upload

---
+++ AIS_2_LAB02_VTIP_302/TSN_DEMO_02_Quadratic/src/main/java/kz/talipovsn/quadratic/MainActivity.java
@@ -0,0 +1,78 @@
+package kz.talipovsn.quadratic;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Локальные переменные для доступа к компонентам окна
+    private EditText editText_a, editText_b, editText_x;
+    private TextView textView_y;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Инициализация переменных доступа к компонентам окна
+        editText_a = findViewById(R.id.editText_a);
+        editText_b = findViewById(R.id.editText_b);
+        editText_x = findViewById(R.id.editText_x);
+        textView_y = findViewById(R.id.textView_y);
+
+        // Проверка на переворот экрана и восстановление нужных значений в компонентах
+        if (savedInstanceState != null) {
+            textView_y.setText(savedInstanceState.getString("y"));
+        }
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Сохранение нужных нам значений компонент при перевороте экрана
+        outState.putString("y", textView_y.getText().toString());
+    }
+
+    // МЕТОД ДЛЯ КНОПКИ РАСЧЕТА
+    @SuppressLint("DefaultLocale")
+    public void onCalc(View v) {
+
+        // Локальные переменные
+        double a, b, x, y;
+
+        try {
+            // Считывание введенных входных значений в переменные
+            a = Double.parseDouble(editText_a.getText().toString());
+            b = Double.parseDouble(editText_b.getText().toString());
+            x = Double.parseDouble(editText_x.getText().toString());
+        } catch (Exception e) {
+            // Выдача всплывающего сообщения на экран об ошибке
+            Toast.makeText(MainActivity.this, "Неверные входные данные!",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Расчет значений x1 и x2
+
+        if (x >= 4) {
+            y = 12 * x / (Math.pow(a, 3) - b);
+        } else {
+            y = 3 * Math.pow(x, 2) - a * b;
+        }
+        if (!Double.isNaN(y) && !Double.isInfinite(y)) {
+            textView_y.setText(String.format("y = %.2f", y));
+        } else {
+            String err = "Деление на ноль невозможно!";
+            textView_y.setText(err);
+        }
+
+    }
+
+}
